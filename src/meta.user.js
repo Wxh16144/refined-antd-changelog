72443600cbd17b4b56200f7343ae82bb6c66bcac
wuxh <wxh1220@gmail.com>
2024-04-01 21:46:32 +0800

    chore: release v1.0.2

--- src/meta.user.js
+++ src/meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Refined Ant Design Changelog
 // @namespace refined-antd-changelog
-// @version      1.0.1
+// @version      1.0.2
 // @description Collapse / mark non-recommended versions to get a clean and fresh journal page
 // @license MIT
 // @author Wuxh <wxh16144@qq.com>
@@ -19,11 +19,11 @@
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
 // @require     https://code.jquery.com/jquery-3.6.1.slim.min.js
-// @require     https://registry.npmmirror.com/refined-antd-changelog/1.0.1/files/index.user.js
+// @require     https://registry.npmmirror.com/refined-antd-changelog/1.0.2/files/index.user.js
 // ==/UserScript==
 
 /**
  * 这里直接写了版本号，因为这个文件是在打包时生成的，
  * 如需审查代码，请移步 https://github.com/Wxh16144/refined-antd-changelog#readme
  */
-;var __rac_version__ = "1.0.1";
+;var __rac_version__ = "1.0.2";
